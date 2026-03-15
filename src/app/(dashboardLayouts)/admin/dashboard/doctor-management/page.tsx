61382f164ce8d7046db43649e83b295c28946453
fahimmuntacher <muntacherfahim234@gmail.com>
2026-03-16 04:28:49 +0600

    doctor management table add using tanstack table

--- src/app/(dashboardLayouts)/admin/dashboard/doctor-management/page.tsx
+++ src/app/(dashboardLayouts)/admin/dashboard/doctor-management/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import DoctorsTable from "@/components/modules/DoctorManagements/DoctorsTable";
+import { getDoctors } from "@/src/services/doctor.services";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import React from "react";
 
-const AdminDoctorManagementPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const AdminDoctorManagementPage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["doctors"],
+    queryFn: getDoctors,
+    staleTime: 1000 * 60 * 60,
+    gcTime: 1000 * 60 * 60 * 6,
+  });
+  return <HydrationBoundary state={dehydrate(queryClient)}>
+    <DoctorsTable></DoctorsTable>
+  </HydrationBoundary>;
 };
 
 export default AdminDoctorManagementPage;
